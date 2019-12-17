f7a6a3e4eb5fc8fb0de51be681ec1b4628410479
aleksanderbrymora <aleksander.australia@gmail.com>
2019-12-17 21:51:32 +1100

    Got the parser working, have to check for xss, todo sync with firebase

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { withAuthorization } from "../Session";
 import loader from "../Resources/loader.gif";
+import Markdown from "markdown-it";
 
 const INITIAL_STATE = {
   isFetchDone: false,
-  title: '',
-  input: ''
+  title: "",
+  input: "",
+  parsed: ""
 };
 
 class HomePage extends Component {
@@ -15,23 +17,45 @@ class HomePage extends Component {
   }
 
   async componentDidMount() {
-    this.setState({isFetchDone: true});
+    this.setState({ isFetchDone: true });
   }
+
+  onChangeTitle = e => this.setState({ title: e.target.value });
+
+  onChangeInput = e => {
+    const md = new Markdown();
+    const parsed = md.render(e.target.value);
+    this.setState({
+      parsed: parsed,
+      input: e.target.value
+    });
+  };
 
   render() {
     if (this.state.isFetchDone) {
       return (
-        <div className={'page'}>
+        <div className={"page"}>
           <div className="left">
-            <input placeholder={'Title'} autoFocus className={'title'} />
+            <input
+              placeholder={"Title"}
+              autoFocus
+              name={"title"}
+              value={this.state.title}
+              className={"title"}
+              onChange={this.onChangeTitle}
+            />
             <textarea
-                placeholder={'Your note...'}
-                className={'user-input'}
-                // rows={'30'}
+              placeholder={"Your note..."}
+              name={"input"}
+              value={this.state.input}
+              className={"user-input"}
+              onChange={this.onChangeInput}
             />
           </div>
           <div className="right">
-            test
+            <h1>{this.state.title}</h1>
+            <div dangerouslySetInnerHTML={{ __html: this.state.parsed }} />
+            {/*<ParsedMarkdown markdown={this.state.parsed} />*/}
           </div>
         </div>
       );
